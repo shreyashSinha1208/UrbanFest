87362fa2952f49b717d339bdaf69c39cb5b237b3
shreyashSinha1208 <shreyash.cs22@bmsce.ac.in>
2025-11-01 19:34:39 +0530

    Signup working

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,10 +1,11 @@
 // routes/userRoutes.js
 import express from 'express';
-import { loginUser, logoutUser, checkUser, updateUserProfile, getCurrentUser } from '../controllers/userController.js';
+import { loginUser, logoutUser, checkUser, updateUserProfile, getCurrentUser, signupUser } from '../controllers/userController.js';
 import { upload } from '../cloudinary.js';
 
 const router = express.Router();
 
+router.post('/signup', signupUser);
 router.post('/login', loginUser);
 router.get('/logout', logoutUser);
 router.get('/check', checkUser);
